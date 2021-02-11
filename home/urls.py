21e38fd224d59b3315094fc01cb6e20d387fd082
stefbez <stefbez@hotmail.com>
2021-02-11 22:13:29 +0000

    Add navbar and header template with temporary logo. Add urls for home app to each info page for gym

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('gym/', views.gym, name='gym'),
+    path('racquets/', views.racquets, name='racquets'),
+    path('swim-spa/', views.swim, name='swim'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+    return render(request, 'home/about.html')
+
+
+def gym(request):
+    """ A view to return the gym page """
+    return render(request, 'home/gym.html')
+
+
+def racquets(request):
+    """ A view to return the racquets page """
+    return render(request, 'home/racquets.html')
+
+
+def swim(request):
+    """ A view to return the swim & spa page """
+    return render(request, 'home/swim.html')
