a635e1de01e037050d8c43f11ba5b683e0e2b3a0
stefbez <stefbez@hotmail.com>
2021-03-07 17:58:05 +0000

    Finish deployment to aws and heroku and set up payment to run on new domain url

--- payment/views.py
+++ payment/views.py
@@ -69,7 +69,7 @@ def stripe_config(request):
 @csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
-        domain_url = 'https://8000-lime-wallaby-so47o6kn.ws-eu03.gitpod.io/'
+        domain_url = 'https://bee-fitness.herokuapp.com/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
             checkout_session = stripe.checkout.Session.create(
