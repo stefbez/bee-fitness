711354b3786ce465b0e3e0c427d6417c1cd36e76
stefbez <stefbez@hotmail.com>
2021-03-07 14:41:05 +0000

    Continue deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bee_fitness.wsgi:application
--- bee_fitness/settings.py
+++ bee_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lde)n8+zq31@l2wh($nbtg4m@32%(0afit-i2#h=l6h=4s5c=s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bee-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,12 +126,17 @@ WSGI_APPLICATION = 'bee_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
