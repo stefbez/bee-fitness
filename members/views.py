26bbae5c327480e82bffa24cca2dd9be3ab6ddd0
stefbez <stefbez@hotmail.com>
2021-03-08 02:56:20 +0000

    Add else statement to add context to members view

--- members/views.py
+++ members/views.py
@@ -8,16 +8,18 @@ from datetime import datetime
 
 @login_required
 def members(request):
-    """ A view to return the index page """
+    """ A view to return the members page """
     exercise_plans = ExercisePlan.objects.filter(user=request.user)
     member_user = request.user
     date = datetime.now()
-    print("Printing", exercise_plans)
     if exercise_plans:
         context = {
             'member_user': member_user,
             'exercise_plans': exercise_plans,
         }
-        print('If statement works')
+    else:
+        context = {
+            'member_user': member_user,
+        }
     date = datetime.strftime(date, '%d %B %Y')
     return render(request, 'members/members.html', context)
